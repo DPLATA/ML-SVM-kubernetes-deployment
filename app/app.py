4b9065f6567b74093462ff5956d3fccffc7f1125
Diego Plata <diegogplata@gmail.com>
2019-12-02 20:41:34 -0600

    css added

--- app/app.py
+++ app/app.py
@@ -3,11 +3,11 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 
 from flask import url_for
-from flask_api import FlaskAPI, status, exceptions
-from models import sessions
-from datetime import datetime
-from models import notes
-from bson import ObjectId
+#from flask_api import FlaskAPI, status, exceptions
+#from models import sessions
+#from datetime import datetime
+#from models import notes
+#from bson import ObjectId
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
@@ -25,5 +25,5 @@ def predict():
 
     return render_template('index.html', prediction_text='Heart disease target {}'.format(prediction[0]))
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
